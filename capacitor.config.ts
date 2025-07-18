6193eb3a4b73a87ebe7747aee70d255a514492cf
Murali Agnihotri <Murali.Agnihotri@gds.ey.com>
2025-07-18 13:06:13 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ooota-food-delivery-app-cap',
+  webDir: 'dist'
+};
+
+export default config;
